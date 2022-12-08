1976f4c24de9e0183c2a12e107d525ea66238fa7
Luis Hidalgo <hid21002@byui.edu>
2022-12-08 13:21:30 -0700

    updates

--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -1,64 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MeleeEnemy: MonoBehaviour
+public class MeleeEnemy : MonoBehaviour
 {
-   [SerializeField]private float attackCooldown;
-   [SerializeField]private float range;
-   [SerializeField]private int damage;
-   [SerializeField]private BoxCollider2D boxCollider;
-   [SerializeField]private LayerMask playerLayer ;
-   [SerializeField]private float colliderDistance;
-   private Animator anim;
-   private Health playerHealth;
+    [Header ("Attack Parameters")]
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private int damage;
 
-   private float cooldoownTimer = Mathf.Infinity;
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private BoxCollider2D boxCollider;
 
-   private void Awake() {
-    anim = GetComponent<Animator>();
-   }
+    [Header("Player Layer")]
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
 
-   private void Update() {
-    cooldoownTimer += Time.deltaTime;
+    //References
+    private Animator anim;
+    private Health playerHealth;
+    private EnemyPatrol enemyPatrol;
 
-    if (PlayerInSight())
+    private void Awake()
     {
-        if (cooldoownTimer >= attackCooldown)
-        {
-            cooldoownTimer = 0;
-            anim.SetTrigger("meleeAttack");
-            
-        }
+        anim = GetComponent<Animator>();
+        enemyPatrol = GetComponentInParent<EnemyPatrol>();
     }
-   }
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        //Attack only when player in sight?
+        if (PlayerInSight())
+        {
+            if (cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+                anim.SetTrigger("meleeAttack");
+            }
+        }
+
+        if (enemyPatrol != null)
+            enemyPatrol.enabled = !PlayerInSight();
+    }
 
     private bool PlayerInSight()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
-        new Vector3(boxCollider.bounds.size.x * range , boxCollider.bounds.size.y, boxCollider.bounds.size.z),0,
-        Vector2.left, 0,
-        playerLayer );
+        RaycastHit2D hit = 
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.left, 0, playerLayer);
 
         if (hit.collider != null)
             playerHealth = hit.transform.GetComponent<Health>();
 
         return hit.collider != null;
     }
-
-    private void OnDrawGizmos() {
+    private void OnDrawGizmos()
+    {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
-        new Vector3(boxCollider.bounds.size.x * range , boxCollider.bounds.size.y, boxCollider.bounds.size.z));
-        
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
 
     private void DamagePlayer()
     {
         if (PlayerInSight())
-        {
             playerHealth.TakeDamage(damage);
-        }
     }
-
 }
