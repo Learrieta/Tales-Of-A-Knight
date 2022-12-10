cdaff52d8b566904e823a4e2247871b17266d5e5
Luis Hidalgo <hid21002@byui.edu>
2022-12-10 06:25:53 -0700

    updates

--- Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -15,6 +15,20 @@ public class PlayerRespawn : MonoBehaviour
 
     private void Respawn() 
     {
-      transform.position = currentCheckpoint.position;        
+      transform.position = currentCheckpoint.position;  
+      playerHealth.Respawn();
+      Camera.main.GetComponent<CameraController>().MoveToNewRoom(currentCheckpoint.parent);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.transform.tag == "Checkpoint")
+        {
+            currentCheckpoint = other.transform;
+            SoundManager.instance.Playsound(checkpointSound);
+            other.GetComponent<Collider2D>().enabled = false;
+            other.GetComponent<Animator>().SetTrigger("appear");
+        }
+        
     }
 }
